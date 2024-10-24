91ae4739991ce1f611d44cdfd86c9077f54cc0fa
iamrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-10-24 07:23:35 +0530

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from '../components/Header'
+import Steps from '../components/Steps'
+import BgSlider from '../components/BgSlider'
+import Testimonals from '../components/Testimonals'
+import Upload from '../components/Upload'
+
+const Home = () => {
+  return (
+    <div>
+    <Header />
+    <Steps />
+    <BgSlider/>
+    <Testimonals />
+    <Upload />
+
+    </div>
+  )
+}
+
+export default Home
---
+++ server/controllers/userController.js
@@ -0,0 +1,6 @@
+// API Controller Function to manage Cler USers in Database
+// http://localhost:4000/api/user/webhooks
+
+const clerkWebhooks = async (req,res) => {
+    
+}
